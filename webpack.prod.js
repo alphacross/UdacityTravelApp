97fe98c8fa844f0b988f6e00cb214f06165e9318
Andreas Henggie <andreas.henggie@bertelsmann.de>
2023-07-25 14:33:59 +0800

    Add Service worker to Prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -36,6 +37,7 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new WorkboxPlugin.GenerateSW({}),
     ]
 }
